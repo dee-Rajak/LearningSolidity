fd3cd81cf0eec5e06d3c84bccb3a35288ea89183
dee-Rajak <iamdeerajak@gmail.com>
2023-09-24 02:16:01 +0530

    Constants

---
+++ Constants.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Constants {
+    address public constant MY_ADDRESS = 0xd9145CCE52D386f254917e481eB44e9943F39138;
+    uint public constant MY_UINT = 123;
+}
